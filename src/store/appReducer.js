ba8fa4c455324ddf25ab66897e65b4b4d400699e
motokikasai <kmomoky@hotmail.com>
2020-06-05 14:25:32 +0200

    Move data & images to src and set up basic store & reducer template

---
+++ src/store/appReducer.js
@@ -0,0 +1,22 @@
+// import { combineReducers } from "redux";
+
+import products from "../data/products.json";
+
+const normalizedProducts = products.reduce((acc, curr) => {
+  acc[curr.id] = curr;
+
+  return acc;
+}, {});
+console.log(normalizedProducts);
+
+function appReducer(state = [], action) {
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+export default appReducer;
