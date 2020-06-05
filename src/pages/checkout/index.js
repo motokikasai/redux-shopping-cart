62474704f96f99c9863cb847c667d4201368582a
motokikasai <kmomoky@hotmail.com>
2020-06-06 00:01:15 +0200

    Add layout to Checkout page

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -12,8 +12,24 @@ class Checkout extends Component {
             <img src={close} className="close-icon" alt="back-icon" />
           </div>
         </Link>
-        <section>{/* each item selected... */}</section>
-        <div className="not-found">Cart is empty. Start shopping now!</div>
+        <section className="product-review">
+          <div className="summary-wrapper">
+            <img src="../img.jpg" alt="product-pic" />
+            <div className="summary-description">
+              <div>Summary product title</div>
+              <div>Price</div>
+              <div>Qty</div>
+            </div>
+          </div>
+          <button className="remove-selection">remove</button>
+        </section>
+        <section className="summary-total">
+          <div className="summary">
+            <p>Total</p>
+            <div className="total-price">4372349727 EURO</div>
+            <button className="checkout-btn">Checkout</button>
+          </div>
+        </section>
       </div>
     );
   }
